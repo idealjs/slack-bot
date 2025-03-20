6cbadd0b50975b38f76f31123f58d0aa8b6bd8a9
JIM-GLITCH <343306431@qq.com>
2025-03-20 22:22:20 +0800

    Add cron job functionality and configuration for Slack messages

--- main.ts
+++ main.ts
@@ -1,14 +1,39 @@
 import { WebClient } from "@slack/web-api";
+import { type } from "arktype";
 import dotenv from "dotenv";
+import fs from "fs"
+import cron from "node-cron"
 dotenv.config();
 const web = new WebClient(process.env.SLACK_TOKEN);
 console.log('Getting started with Node Slack SDK');
 
+const CronJob = type({
+    cron: type.string.narrow(cron.validate),
+    channel: "string",
+    text: "string"
+})
+
+const CronJobArray = CronJob.array()
+
 const currentTime = new Date().toTimeString();
 async function main() {
-    await web.chat.postMessage({
-        channel: '#slack-bot-project',
-        text: "下一步该做什么"
+    // 读取配置运行定时任务
+    const cronTableJsonStr = fs.readFileSync('cronTable.json', 'utf8')
+    const jobs = CronJobArray.assert(JSON.parse(cronTableJsonStr))
+    jobs.forEach(x=>{
+        cron.schedule(x.cron, async () => {
+            console.log(`[${currentTime}] Sending message to channel ${x.channel}`)
+            try {
+                const result = await web.chat.postMessage({
+                    channel: x.channel,
+                    text: x.text,
+                });
+                console.log(`Successfully send message ${result.ts} in conversation ${x.channel}`);
+            } catch (error) {
+                console.error(`Error posting message to ${x.channel}: ${error}`);
+            }
+        })
     })
+
 }
 main()
