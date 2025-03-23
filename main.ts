779dc38094c8b22743214fff8f3f114f28dc0323
chenjunyao <343306431@qq.com>
2025-03-24 01:01:12 +0800

    Add Slack Bolt integration and update dependencies

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
-import { WebClient } from "@slack/web-api";
+import { App, FileInstallationStore } from "@slack/bolt";
 import { type } from "arktype";
+import cron from "node-cron";
+import fs from "node:fs";
 import dotenv from "dotenv";
-import fs from "fs"
-import cron from "node-cron"
 dotenv.config();
-const web = new WebClient(process.env.SLACK_TOKEN);
-console.log('Getting started with Node Slack SDK');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    clientId: process.env.SLACK_CLIENT_ID,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    // installationStore: new FileInstallationStore()
+})
 
 const CronJob = type({
     cron: type.string.narrow(cron.validate),
@@ -15,25 +22,40 @@ const CronJob = type({
 
 const CronJobArray = CronJob.array()
 
-const currentTime = new Date().toTimeString();
 async function main() {
-    // 读取配置运行定时任务
-    const cronTableJsonStr = fs.readFileSync('cronTable.json', 'utf8')
-    const jobs = CronJobArray.assert(JSON.parse(cronTableJsonStr))
-    jobs.forEach(x=>{
-        cron.schedule(x.cron, async () => {
-            console.log(`[${currentTime}] Sending message to channel ${x.channel}`)
-            try {
-                const result = await web.chat.postMessage({
-                    channel: x.channel,
-                    text: x.text,
-                });
-                console.log(`Successfully send message ${result.ts} in conversation ${x.channel}`);
-            } catch (error) {
-                console.error(`Error posting message to ${x.channel}: ${error}`);
-            }
-        })
+    app.message(async ({ message, say }) => {
+        console.log(message)
     })
 
+        //     channel:"#slack-bot-project",
+    // 读取配置运行定时任务
+    // const cronTableJsonStr = fs.readFileSync('cronTable.json', 'utf8')
+    // const jobs = CronJobArray.assert(JSON.parse(cronTableJsonStr))
+    // jobs.forEach(x=>{
+    //     cron.schedule(x.cron, async () => {
+    //         console.log(`[${currentTime}] Sending message to channel ${x.channel}`)
+    //         try {
+    //             const result = await web.chat.postMessage({
+    //                 channel: x.channel,
+    //                 text: x.text,
+    //             });
+    //             console.log(`Successfully send message ${result.ts} in conversation ${x.channel}`);
+    //         } catch (error) {
+    //             console.error(`Error posting message to ${x.channel}: ${error}`);
+    //         }
+    //     })
+    // })
+    // webClient.chat.postMessage({
+    //     channel:"#slack-bot-project",
+    //     // reply_broadcast:false,
+    //     text:"hello world",
+    //     // thread_ts:"1631087304.000100"
+    // })
+    await app.start(process.env.PORT || 3000);
+    app.logger.info('⚡️ Bolt app is running!');
+    app.client.chat.postMessage({
+        channel: "#slack-bot-project",
+        text: "hello world"
+    })
 }
 main()
